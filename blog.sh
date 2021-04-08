15905cce32a1d7f66e1332438f701086c91ccce2
wuhuanda <583783099@qq.com>
2021-04-08 11:26:47 +0800

    init

---
+++ blog.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+WORK_PATH='/workplace/blog'
+cd $WORK_PATH
+echo "清理代码"
+git reset --hard origin/master
+git clean -f
+echo "拉取最新代码"
+git pull origin master
+echo "打包最新代码"
+npm run build
+echo "开始构建镜像"
+docker build -t blog:1.0
+echo "删除旧容器"
+docker stop blog
+docker rm blog
+echo "启动新容器"
+docker container run -p 80:80 -d --name blog -d blog:1.0
